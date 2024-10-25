d158e0681b1b057e7b394c443247d1e8c5203877
dao <146765250+ssaumdokak@users.noreply.github.com>
2024-10-25 11:57:13 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
+// обробник подій, який відстежує завантаження контенту
 document.addEventListener('DOMContentLoaded', function(){
     const btnOpenModal = document.querySelector('#btnOpenModal');
     const modalBlock = document.querySelector('#modalBlock');
     const closeModal = document.querySelector('#closeModal');
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
-    const prevBtn = document.querySelector('#prev');
-    const nextBtn = document.querySelector('#next');
-    
+    const nextButton = document.querySelector('#next');
+    const prevButton = document.querySelector('#prev');
+    const sendButton = document.querySelector('#send');
+
+// об'єкт, що містить питання та відповіді
     const questions = [
         {
             question: "Якого кольору бургер?",
@@ -41,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function(){
             type: 'radio'
         },
         {
-            question: "Додаткові інгредієнти ?",
+            question: "Додаткові інгредієнти?",
             answers: [
                 {
                     title: 'Помідор',
@@ -82,63 +85,128 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     ];
 
-    btnOpenModal.addEventListener('click', ()=> {
+    // обробники подій відкриття/закриття модального вікна
+    btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
-        openTest();
-    })
+        playTest();
+    });
 
     closeModal.addEventListener('click', () => {
         modalBlock.classList.remove('d-block');
-    })
+    });
 
-    const openTest = () => {
+    // функція запуску тестування
+    const playTest = () => {
+
+        const finalAnswers = [];
+
+        // змінна з номером питання
         let numberQuestion = 0;
 
-        const toggleButtons = () => {
-            if (numberQuestion===0) {
-                prevBtn.style.display = "none";
-            } else {
-                prevBtn.style.display = "block";
-            }
-
-            if (numberQuestion===questions.length-1) {
-                nextBtn.style.display = "none";
-            } else {
-                nextBtn.style.display = "block";
-            }
-        }
-
-        const generateAnswers = (index) => {
+        // функція рендерингу відповідей
+        const renderAnswers = (index) => {
             questions[index].answers.forEach((answer) => {
                 const answerItem = document.createElement('div');
-                answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+
+                answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center');
 
                 answerItem.innerHTML = `
-                    <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+                    <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
                     <label for="${answer.title}" class="d-flex flex-column justify-content-between">
                     <img class="answerImg" src="${answer.url}" alt="burger">
                     <span>${answer.title}</span>
-                    </label>`
-
+                    </label>
+                `;
                 formAnswers.appendChild(answerItem);
-            })
+            });
         }
-        const generateQuestions = (indexQuestion) => {
-            formAnswers.innerHTML = ``;
-            questionTitle.textContent = `${questions[indexQuestion].question}`;
-            generateAnswers(indexQuestion);
-            toggleButtons();
-        }
-        generateQuestions(numberQuestion);
 
-        nextBtn.onclick = () => {
+        // функція рендерингу питань + відповідей
+        const renderQuestions = (indexQuestion) => {
+            formAnswers.innerHTML = '';
+
+            switch (true) {
+                case numberQuestion === 0:
+                    prevButton.classList.add('d-none');
+                    break;
+                case numberQuestion <= questions.length - 1:
+                    prevButton.classList.remove('d-none');
+                    nextButton.classList.remove('d-none');
+                    sendButton.classList.add('d-none');
+                    break;
+                case numberQuestion === questions.length:
+                    prevButton.classList.add('d-none');
+                    nextButton.classList.add('d-none');
+                    sendButton.classList.remove('d-none');
+                    break;
+                default: break;
+            }
+
+            if(numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+                questionTitle.textContent = `${questions[indexQuestion].question}`;
+                renderAnswers(indexQuestion);
+            }
+
+            if(numberQuestion === questions.length) {
+                nextButton.classList.add('d-none');
+                prevButton.classList.add('d-none');
+                sendButton.classList.remove('d-none');
+
+                formAnswers.innerHTML = `
+                    <div class="form-group">
+                        <label for="numberPhone">Введіть свій номер</label>
+                        <input type="phone" class="form-control" id="numberPhone">
+                    </div>
+                `;
+            }
+
+            if(numberQuestion === questions.length + 1) {
+                formAnswers.textContent = 'Дякуємо за пройдений тест!';
+                setTimeout(() => {
+                    modalBlock.classList.remove('d-block');
+                }, 2000);
+            }
+
+        } 
+
+        // запуск функції рендерингу 
+        renderQuestions(numberQuestion);  
+
+        const checkAnswer = () => {
+            const obj = {};
+            const inputs = [...formAnswers.elements].filter((input) => input.checked || input.id === 'numberPhone')
+
+            inputs.forEach((input, index) => {
+               if(numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+                obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+               }
+
+               if(numberQuestion === questions.length) {
+                obj['Номер телефону'] = input.value;
+               }
+            });
+
+            finalAnswers.push(obj);
+        }
+
+        // обробники подій кнопок next і prev
+        nextButton.onclick = () => {
+            checkAnswer();
             numberQuestion++;
-            generateQuestions(numberQuestion);
+            renderQuestions(numberQuestion);
         }
-        prevBtn.onclick = () => {
+
+        prevButton.onclick = () => {
             numberQuestion--;
-            generateQuestions(numberQuestion);
+            renderQuestions(numberQuestion);
         }
-        toggleButtons();
+
+        sendButton.onclick = () => {
+            checkAnswer();
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+            console.log(finalAnswers);
+        }
     }
+
 })
